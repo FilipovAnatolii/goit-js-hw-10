8ea8e0b7fc9899c1564b3be752861e22aeb8b58e
Filipov Anatolii <anatolii.filippov@icloud.com>
2025-10-04 10:53:21 +0300

    js-10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,16 +1,16 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-
-
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
-
-iziToast.show({
-});
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 let userSelectedDate = null;
-let timerId = null;
+let timerInterval = null;
+const button = document.querySelector('button[data-start]');
+const selectedInput = document.querySelector('#datetime-picker');
+const timerResult = document.querySelector('.timer');
+
+button.disabled = true;
+button.addEventListener('click', timerStart);
 
 const options = {
   enableTime: true,
@@ -18,76 +18,78 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const pickedDate = selectedDates[0];
-
-    if (pickedDate <= new Date()) {
+    if (Date.now() < Date.parse(selectedDates[0])) {
+      userSelectedDate = selectedDates[0];
+      button.disabled = false;
+    } else {
+      button.disabled = true;
       iziToast.error({
-        title: "Please choose a date in the future",
-        position: "topRight",
-        messageColor: "#fff",
-        titleColor: "#fff",
-        backgroundColor: "#ef4040"
+        message: 'Please choose a date in the future.',
+        position: 'topRight',
       });
-      userSelectedDate = null;
-      startBtn.disabled = true;
-      return;
     }
-    userSelectedDate = pickedDate;
   },
 };
 
-flatpickr("#datetime-picker", options);
+flatpickr(selectedInput, options);
 
-const startBtn = document.querySelector("button[data-start]");
-const daysEl = document.querySelector("[data-days]");
-const hoursEl = document.querySelector("[data-hours]");
-const minutesEl = document.querySelector("[data-minutes]");
-const secondsEl = document.querySelector("[data-seconds]");
+function timerStart() {
+  if (timerInterval !== null) return;
+  triggersDisable();
+  timerInterval = setInterval(() => {
+    const resultTime = userSelectedDate - Date.now();
 
-startBtn.addEventListener("click", () => {
-  if (!userSelectedDate) {
-    alert("Please select a valid date first!");
-    return;
-  }
-
-
-  if (timerId) clearInterval(timerId);
-
-  timerId = setInterval(() => {
-    const diff = userSelectedDate - new Date();
-
-    if (diff <= 0) {
-      clearInterval(timerId);
-      alert("Countdown finished!");
+    if (resultTime < 0) {
+      stopTimer();
       return;
     }
 
-    const { days, hours, minutes, seconds } = convertMs(diff);
-
-    daysEl.textContent = String(days).padStart(2, "0");
-    hoursEl.textContent = String(hours).padStart(2, "0");
-    minutesEl.textContent = String(minutes).padStart(2, "0");
-    secondsEl.textContent = String(seconds).padStart(2, "0");
+    updateTimer(convertMs(resultTime));
   }, 1000);
-});
+}
 
 function convertMs(ms) {
-
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function updateTimer({ days, hours, minutes, seconds }) {
+  timerResult.querySelector('[data-days]').textContent = addLeadingZero(days);
+  timerResult.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+  timerResult.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
+  timerResult.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
+}
 
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+  triggersEnable();
+  updateTimer({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+}
+
+function triggersDisable() {
+  button.disabled = true;
+  selectedInput.disabled = true;
+}
+
+function triggersEnable() {
+  selectedInput.disabled = false;
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
